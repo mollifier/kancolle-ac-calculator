6a4e2339dd405fc1e5dfac7f2786a77f3bc2a509
mollifier <mollifier@gmail.com>
2016-06-06 23:53:22 +0900

    海域モデル作成
    issue #5

---
+++ kancolle-ac-calculator/StageCreater.swift
@@ -0,0 +1,60 @@
+//
+//  StageCreater.swift
+//  kancolle-ac-calculator
+//
+//  Created by mollifier on 2016/06/07.
+//
+//
+
+import Foundation
+
+class StageCreater {
+    private static let _stages: [Stage] = [
+        Stage(areaNumber: 1, stageNumber: 1, stageType: .Normal, cost: 150),
+        Stage(areaNumber: 1, stageNumber: 1, stageType: .Pursuit, cost: 100),
+        Stage(areaNumber: 1, stageNumber: 2, stageType: .Normal, cost: 200),
+        Stage(areaNumber: 1, stageNumber: 2, stageType: .Pursuit, cost: 100),
+        Stage(areaNumber: 1, stageNumber: 3, stageType: .Normal, cost: 240),
+        Stage(areaNumber: 1, stageNumber: 3, stageType: .Pursuit, cost: 150),
+        Stage(areaNumber: 1, stageNumber: 4, stageType: .Normal, cost: 280),
+        Stage(areaNumber: 1, stageNumber: 4, stageType: .Pursuit, cost: 150),
+    
+        Stage(areaNumber: 2, stageNumber: 1, stageType: .Normal, cost: 240),
+        Stage(areaNumber: 2, stageNumber: 1, stageType: .Pursuit, cost: 150),
+        Stage(areaNumber: 2, stageNumber: 2, stageType: .Normal, cost: 260),
+        Stage(areaNumber: 2, stageNumber: 2, stageType: .Pursuit, cost: 150),
+        Stage(areaNumber: 2, stageNumber: 3, stageType: .Normal, cost: 280),
+        Stage(areaNumber: 2, stageNumber: 3, stageType: .Pursuit, cost: 200),
+        Stage(areaNumber: 2, stageNumber: 4, stageType: .Normal, cost: 300),
+        Stage(areaNumber: 2, stageNumber: 4, stageType: .Pursuit, cost: 200),
+    
+        Stage(areaNumber: 3, stageNumber: 1, stageType: .Normal, cost: 360),
+        Stage(areaNumber: 3, stageNumber: 1, stageType: .Pursuit, cost: 150),
+        Stage(areaNumber: 3, stageNumber: 2, stageType: .Normal, cost: 390),
+        Stage(areaNumber: 3, stageNumber: 2, stageType: .Pursuit, cost: 150),
+        Stage(areaNumber: 3, stageNumber: 3, stageType: .Normal, cost: 420),
+        Stage(areaNumber: 3, stageNumber: 3, stageType: .Pursuit, cost: 200),
+        Stage(areaNumber: 3, stageNumber: 4, stageType: .Normal, cost: 450),
+        Stage(areaNumber: 3, stageNumber: 4, stageType: .Pursuit, cost: 200),
+    ]
+
+    static func areaCount() -> Int {
+        return 3
+    }
+    
+    static func stages() -> [Stage] {
+        return self._stages
+    }
+    
+    static func satges(areaNumber: Int) -> [Stage] {
+        return self.stages().filter { (s) in s.areaNumber == areaNumber }
+    }
+
+    static func satges(stageType: Stage.StageType) -> [Stage] {
+        return self.stages().filter { (s) in s.stageType == stageType }
+    }
+    
+    static func satges(areaNumber: Int, stageType: Stage.StageType) -> [Stage] {
+        return self.stages().filter { (s) in s.areaNumber == areaNumber && s.stageType == stageType }
+    }
+}
