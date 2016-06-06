8ae93e8bef7d0c98e08bedada0a0b3574b364ddf
mollifier <mollifier@gmail.com>
2016-06-06 21:10:36 +0900

    iOSアプリケーション プロジェクト新規作成
    issue #1

---
+++ kancolle-ac-calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kancolle-ac-calculator
+//
+//  Created by mollifier on 2016/06/06.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
